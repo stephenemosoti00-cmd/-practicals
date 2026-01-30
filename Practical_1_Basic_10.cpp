e667227d5273d04921bc66ed416b380458d38c1c
stephenemosoti00-cmd <stephenemosoti00@gmail.com>
2026-01-30 23:26:28 +0300

    Create Practical_1_Basic_10.cpp

---
+++ Practical_1_Basic_10.cpp
@@ -0,0 +1,18 @@
+Copy code
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int age;
+
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Hello " << name << "! You are " << age << " years old." << endl;
+
+    return 0;
+}
